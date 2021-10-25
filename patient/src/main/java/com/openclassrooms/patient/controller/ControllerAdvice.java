e92a819a6669a6320a2c083be3e99c9692e696e3
GBOUZAZI <ghazi.bouzazi@hardis-group.com>
2021-10-25 21:06:25 +0200

    setup controller advice Patient microservice

---
+++ patient/src/main/java/com/openclassrooms/patient/controller/ControllerAdvice.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.patient.controller;
+
+import com.openclassrooms.patient.exception.PatientNotFoundExecption;
+import com.openclassrooms.patient.exception.RdvNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(PatientNotFoundExecption.class)
+    @ResponseBody
+    public ResponseEntity<Object> handlePatientNotFoundExecption(PatientNotFoundExecption ex, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Patient Not Found!!!" + ex);
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RdvNotFoundException.class)
+    @ResponseBody
+    public ResponseEntity<Object> handleRdvNotFoundException(RdvNotFoundException ex, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "RDV Not Found!!!"+ ex);
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+}
